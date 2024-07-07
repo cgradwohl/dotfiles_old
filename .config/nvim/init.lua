b0be6f307b3bc6e94898011270dd670940fe7274
Chris Gradwohl <15326707+cgradwohl@users.noreply.github.com>
2024-07-07 11:18:09 -0700

    rm nvim configs

--- .config/nvim/init.lua
+++
@@ -1,9 +0,0 @@
--- bootstrap lazy.nvim, LazyVim and your plugins
-require("config.lazy")
-
--- set shell to be installed bash
-vim.o.shell = "/opt/homebrew/bin/bash"
-
--- disable ruby and perl providers
-vim.g.loaded_ruby_provider = 0
-vim.g.loaded_perl_provider = 0
